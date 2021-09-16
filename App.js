e129be46b9fd6c8a2bd469387c07071c43c2e0e4
Tausif Sheikh <63940752+TausifM@users.noreply.github.com>
2021-09-16 11:10:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import { useState, useCallback } from 'react';
+import './App.css';
+
+function App() {
+  const [search, setSearch ] = useState([]);
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if(timer) clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+        func.apply(context, args);
+      }, 500)
+    }
+  }
+  const handleChange = (event) => {
+    const {value} = event.target;
+    fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+    .then(res => res.json())
+    .then(json=> setSearch(json.data.items));
+  }
+  const optimisedVersion = useCallback(debounce(handleChange),[]);
+  
+  return (
+    <div className="App">
+      <input type={'text'} name={'search'} placeholder={'Enter Something...'}
+      className={'search'} onChange={optimisedVersion} /> 
+      {search?.length > 0 && 
+      <div className={'autocomplete'}>
+        {search?.map((el, item) => 
+        <div key={item} className={'autocompleteItems'}>
+              <span>
+                {el.name}
+              </span>
+        </div>
+        )}
+      </div>
+      }
+    </div>
+  );
+}
+
+export default App;
